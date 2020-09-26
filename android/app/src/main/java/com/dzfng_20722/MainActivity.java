02cdde7f606546b6aefa37de12e7431160b4d3a4
Crowdbotics <team@crowdbotics.com>
2020-09-26 10:37:26 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dzfng_20722/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dzfng_20722;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "dzfng_20722";
+    }
+}
